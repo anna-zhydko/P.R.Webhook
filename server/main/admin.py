eabb753030aaf0d7f7b8a52bc6463a97073f7ac6
anna.zhydko <anna.zhydko@krakentechnologies.ltd>
2023-08-11 18:41:28 +0100

    Display received webhook in the django admin page

--- server/main/admin.py
+++ server/main/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import PullRequest
 
 # Register your models here.
+
+
+class PullRequestAdmin(admin.ModelAdmin):
+    list_display = ["number", "title", "author", "link", "updated_at", "review_requested"]
+
+
+admin.site.register(PullRequest, PullRequestAdmin)
