d4b5e41745ea560ab2ff720504c9760c31beb34f
anna.zhydko <anna.zhydko@krakentechnologies.ltd>
2023-08-11 17:13:54 +0100

    Add simple model for review-requested PRs

--- server/main/models.py
+++ server/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class PullRequest(models.Model):
+    number = models.CharField(max_length=20)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=50)
+    link = models.CharField(max_length=200)
+    updated_at = models.CharField(max_length=100)
+    review_requested = models.TextField()
