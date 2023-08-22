35b0fe7ccf5708884dc1765b5e2c392bd3e09884
anna.zhydko <anna.zhydko@krakentechnologies.ltd>
2023-08-22 15:23:45 +0100

    Add 'claimed' column. Make links clickable in admin page. Make 'link' and 'number' fields unique

--- server/main/models.py
+++ server/main/models.py
@@ -4,9 +4,10 @@ from django.db import models
 
 
 class PullRequest(models.Model):
-    number = models.IntegerField()
+    number = models.IntegerField(unique=True)
     title = models.CharField(max_length=200)
     author = models.CharField(max_length=50)
-    link = models.CharField(max_length=200)
+    link = models.CharField(max_length=200, unique=True)
     updated_at = models.CharField(max_length=100)
     review_requested = models.TextField()
+    claimed = models.CharField(max_length=200, blank=True, default="")
