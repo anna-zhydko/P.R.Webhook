bbdd4c3c7a728ef0adaa94eb3263296eff5846ee
anna.zhydko <anna.zhydko@krakentechnologies.ltd>
2023-09-25 11:36:24 +0100

    Change url for handling the github webhooks
    Prior to this change, we catch the webhook on the main url that gave us
    an error when we just tried to load django main page. That was because
    djano expected to receive a webhook post request.
    
    This change add specific url only for the webhooks.

--- server/main/urls.py
+++ server/main/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.handle_webhook_event, name='handle_webhook_event'),
+    path('webhook/', views.handle_webhook_event, name='handle_webhook_event'),
 ]
