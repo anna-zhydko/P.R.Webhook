d580ad17fca5c65941100caca15646301b1e8c4f
anna.zhydko <anna.zhydko@krakentechnologies.ltd>
2023-11-19 18:34:53 +0000

    Add ReviewerRequested and TeamRequested tables
    Prior to this change,
    
    This change

--- server/main/models.py
+++ server/main/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class ReviewerRequested(models.Model):
+    login = models.CharField(max_length=100)
+    github_id = models.IntegerField(unique=True)
+    url = models.CharField(max_length=200, unique=True)
+
+
+class TeamRequested(models.Model):
+    name = models.CharField(max_length=200)
+    github_id = models.IntegerField(unique=True)
+    slug = models.CharField(max_length=100)
+    url = models.CharField(max_length=200, unique=True)
+    description = models.TextField()
 
 
 class PullRequest(models.Model):
@@ -9,5 +21,6 @@ class PullRequest(models.Model):
     author = models.CharField(max_length=50)
     link = models.CharField(max_length=200, unique=True)
     updated_at = models.CharField(max_length=100)
-    review_requested = models.TextField()
+    reviewer_requested = models.ManyToManyField(ReviewerRequested, blank=True)
+    team_requested = models.ManyToManyField(TeamRequested, blank=True)
     claimed = models.CharField(max_length=200, blank=True, default="")
